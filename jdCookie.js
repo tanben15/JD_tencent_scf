5376f61b2e24dbef60793f8a72b175b48e850647
tanben15 <85775717+tanben15@users.noreply.github.com>
2021-10-23 14:57:11 +0800

    Add files via upload

--- jdCookie.js
+++ jdCookie.js
@@ -3,21 +3,21 @@
  */
 //此处填写京东账号cookie。
 let CookieJDs = [
-  'pt_key=AAJhLg1hADDmz39RB6ZgBgWXi8LvJb0BlRVXCjBGLFOcP18F-gvqN307ATPmyH5wTY_VStsDDK0; pt_pin=tanben117;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=AAJhMg6bADDYickCZdEugQ0TrJhKQ7MLryTUopeLsa2QdnZEYMrFwRWh-24cmP8NTrRvIGZbHS0; pt_pin=jd_5a2ee9240e8f2;',//账号二ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=AAJhMg9fADBCRsOg-ofmkGtoSg_tJihSrmN8mdIKTurfBEAyB1gZl6Olies3wt3vfCWD1j0V324; pt_pin=jd_6245947f92d0a;',//账号三ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=AAJhLixrADCH6wTQtnOsw_jTJGdHL95xAeoLl0C9XAkN9BkC5z_vZKbk0RZdrRASRl7kS9YHROI; pt_pin=157316-83139169;',//账号四ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=AAJhJlPbADA1go7jvAZrl1GHvvjJEpSzs8KinlcI56Is4zWs9XTOKEPuZxcOZJub9AUMTCuqI1Y; pt_pin=303041182-346709;',//账号五ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=AAJhLsjOADAI_xsnno-BkgTrO7b5jNJbNbLuelzAEXOyx7iZyZNSVQMmX1zp0Q80UgT3Sf6uZ5c; pt_pin=jd_6b95ab59f33bc;',//账号六ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=AAJhLslhADCOsU3PuBsX5ZtWKWN-ni-GSo-5Tafz6z7BrlRZwrxFS8nsiqwfRiBmVs6WEcxJI1E; pt_pin=80272529-417959;',//账号七ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=AAJhLsf_ADDPUw5a_Rs5Yyi86xVjIvbfDKP7WbI4-3-wDqAyh98jyBEdorjOxj0wUTIdo2lLd0Q; pt_pin=zhanglianf_mru;',//账号八ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=AAJhLsdqADAy3vCDioWLDcOICo8OndOj-7exxghbLkOJ2iW5gV7Bkqe7dFpwrsPYg3LsTD9p564; pt_pin=jd_FBxosKoLCPaC;',//账号九ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=AAJhLshzADBWw8SjYQSlc0Hf97QGtp7Zvwi8dJm8VwyvPRNmQV9AH_ebmYkNlgHY920H-z6aJI8; pt_pin=fan_1023;',//账号十ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=AAJhLg30ADBummB-Ngkn9LyiQrPJl406sKXSjtGN0DbK9kKVx5xzEsOw-R4W13VodZens_9bT9E; pt_pin=jd_yEQhGVQczrbb;',//账号十一ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=AAJhFQ5NADB7cRY3QO7A_ZD3FCdKe4y9c9iCOEteN2Y7TyjuUQLvP3vJkyu5DVbYAFw18lU05Ao; pt_pin=jd_4ab847068b14c;',//账号十二ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=AAJhGyloADCRROTyvJKvXZaxb2RqHHop_RWTeEanWWpQlXrBuIZTGDcvB3OmwLEJf_3y-2VYbD4; pt_pin=diannaobai_m;',//账号十三ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=AAJhFQjpADBw7lzMqHFL9DYCjLwr2namtQTBYQsLkmp_qG-YDO0STdD0CoheF2d2qnJeNS1Mwks; pt_pin=jd_526d52a9b9b47;',//账号十四ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=AAJhMHvKADCjIJTs1DHZBb4sw9VGJcbTWIczzD9NpfCzULsaEEqu2VLW5ou3YNt8XFvy4X1m0xo; pt_pin=jd_XzTZqoNXlxjL;',//账号十四ck,例:pt_key=XXX;pt_pin=XXX;'',//账号十五ck,例:pt_key=XXX;pt_pin=XXX;'',  如有更多,依次类推
+  'pt_key=AAJhZjHFADB_3e3-sJQcQZbj9oUo6foEtommQBntT6TIYItNj4-umst0a_qKNQ-hg7mU8dPzofQ; pt_pin=tanben117;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhWbfJADDBKpRVvLtTAe1-XgD1wiphrz-6iux44d53yo3JdcyIje59NRQ3YzoZVKmfidfuswE; pt_pin=jd_5a2ee9240e8f2;',//账号二ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhWbktADDA6Fss3HRtG70wskW7YkWm2NXzsNa63VrcEhViHE1p0hLmbb4duEyCad44z9n9w_U; pt_pin=jd_6245947f92d0a;',//账号三ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhV-E6ADDBCKAqI7qX8YabEuH3CnWtCCgtHghL-tJEkFQorwWP8NeuJ1HhDUsByfCbXS_K34U; pt_pin=303041182-346709;',//账号四ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhV-E6ADDBCKAqI7qX8YabEuH3CnWtCCgtHghL-tJEkFQorwWP8NeuJ1HhDUsByfCbXS_K34U; pt_pin=303041182-346709;',//账号五ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhVp2IADAQ1i15WgBSLTDsWsyHVVluFh8DIaSPWuddfxcP6PmGXJuY-C9l6kcejZ220zer5r8; pt_pin=jd_6b95ab59f33bc;',//账号六ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhVnNLADAVrDJxzkIpoaBAtNMj9eYbxGgTk7u_6YNW0H5U9veihblkggHz9RpO7tydyi7ja9k; pt_pin=80272529-417959;',//账号七ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhTCd_ADCg613maxlNj6d_0yUep3eBVMlRg5QcV2TAqebZAGBH-ZrZG9JW4BTqlveDy0rVUPo; pt_pin=zhanglianf_mru;',//账号八ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhVl2qADA7Zk7emC0qucroRLmoYTMGAWj2LJUdxU7XNQ7rxqD1DlnrYBHb4Y2I8EUDVwUQROM; pt_pin=jd_FBxosKoLCPaC;',//账号九ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhX-IkADB9D1vcvpsay5mC1BDyEM3l0RbBxhec9ifiBZ0U_YX7zaDjQ0TfqzmA5mtQhRlumes; pt_pin=fan_1023;',//账号十ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhVlrqADD8EbB-uVzeY80oBRLAsbDicTbgJNsSLowfSanw2R_rb-n-_gqXyAJPaenj4WWPxN0; pt_pin=jd_yEQhGVQczrbb;',//账号十一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhbRzBADCik98tQHC6A7yTvWYXhFsWmU5u87fcMElQ36591dohS6hskrEReKvjiM2HfG3lrw0; pt_pin=jd_4ab847068b14c;',//账号十二ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhbfidADDYE39eK1PtFaK2F6y8_MJmIouiGx75lT5z1wEmTBZqOPa8L0MKxzDhkvNsSaDq7c0; pt_pin=diannaobai_m;',//账号十三ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhah3TADA9OS-KKTRf_U9dxvF4uTuTdIqZazi7zkvV4PKh9z09yeXeSLCLjzun5dX-Az6uBW4; pt_pin=jd_5bf9dca1358b0;',//账号十四ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhYBV5ADC9vspSaL-rlE1WXBVL8Brwa9hUDpn21zSzPFuMFsZCgJ7Yv0nhzMczcoTl06PPvz0; pt_pin=jd_XzTZqoNXlxjL;',//账号十四ck,例:pt_key=XXX;pt_pin=XXX;'',//账号十五ck,例:pt_key=XXX;pt_pin=XXX;'',  如有更多,依次类推
 ]
 // 判断环境变量里面是否有京东ck
 if (process.env.JD_COOKIE) {
